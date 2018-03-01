1ebc2f801ffdc2ef8e704e9b3881e6e4e6dda32e
ZaprudnovAA <aleksey.zaprudnov@yandex.ru>
2018-03-01 23:37:13 +0300

    New project

---
+++ ZAAUniversalUpdaterConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace ZAAUniversalUpdaterConsole
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var process = args[1].Replace(".exe", "");
+
+                foreach (Process proc in Process.GetProcessesByName(process))
+                {
+                    Console.WriteLine("Terminate process!");
+                    proc.Kill();
+                    Thread.Sleep(300);
+                }
+
+                if (File.Exists(args[1]))
+                {
+                    File.Delete(args[1]);
+                }
+
+                if (File.Exists(args[0]))
+                {
+                    File.Move(args[0], args[1]);
+                }
+
+
+                Console.WriteLine("Starting " + args[1]);
+                Process.Start(args[1]);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+}
